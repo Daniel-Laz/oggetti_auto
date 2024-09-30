f44318a53fd559689f458b33d2928f7f08393f71
Lazzaroni.21130 <lazzaroni.daniel.studente@itispaleocapa.it>
2024-09-30 12:39:47 +0200

    creazione oggetto

--- src/auto/Auto.java
+++ src/auto/Auto.java
@@ -2,4 +2,41 @@ package auto;
 
 public class Auto {
 
+    private String marca;
+    private String modello;
+    private double prezzo;
+
+    public void setMarca(String newMarca){
+        marca = newMarca;
+    }
+    public void setModello(String newModello){
+        modello = newModello;
+    }
+    public void setMarca(double newPrezzo){
+        prezzo = newPrezzo;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+    public String getModello(){
+        return modello;
+    }
+    public double getPrezzo(){
+        return prezzo;
+    }
+
+    public Auto(){
+        // non necessario
+        marca = null;
+        modello = null;
+        prezzo = 0;
+    }
+
+    public Auto(String newMarca, String newModello, double newPrezzo){
+        marca = newMarca;
+        modello = newModello;
+        prezzo = newPrezzo;
+    }
+
 }
--- src/auto/Main.java
+++ src/auto/Main.java
@@ -1,7 +1,24 @@
 package auto;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n;
+        int contatore = 0;
+        do{
+            System.out.println("0 - termina programma" +
+                    "1 - aggiungi auto" +
+                    "2 - modifica dati auto" +
+                    "3 - visualizza dati auto");
+            n = in.nextInt();
+            switch(n){
+                case 1:
+                    System.out.println("inserisci i seguenti dati dell'auto");
 
+
+            }
+        }while(n != 0);
     }
 }
